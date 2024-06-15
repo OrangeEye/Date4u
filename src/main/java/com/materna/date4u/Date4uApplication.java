8527a91504520548132270a4bedb739c9f856311
Lasse Ganske <lasse.ganske@googlemail.com>
2024-06-15 19:23:39 +0200

    Kommentare zum ApplicationContext geschrieben

--- src/main/java/com/materna/date4u/Date4uApplication.java
+++ src/main/java/com/materna/date4u/Date4uApplication.java
@@ -1,13 +1,55 @@
 package com.materna.date4u;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class Date4uApplication {
 
+	// slf4j ist nur eine Fassade. Das konkrete Logging-Framework dahinter kann ausgetauscht werden.
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	private String id = UUID.randomUUID().toString().substring(0, 10);
+
+	public Date4uApplication() {
+
+	}
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(Date4uApplication.class, args);
+
+		//1. Alternative die App zu starten. Kann dadurch vorkonfiguriert werden
+//        SpringApplication app = new SpringApplication(Date4uApplication.class);
+//        app.setHeadless(false); // Bei GUI-Anwendungen muss die Anwendung im Headless-Mode gestartet werden
+//        app.setBannerMode(Banner.Mode.OFF); //Wird von den application.properties überschrieben
+//        app.setLogStartupInfo(false);
+//        app.run(args);
+
+		// 2. Alternative: Builder Pattern
+//        ConfigurableApplicationContext context = new SpringApplicationBuilder(Date4uApplication.class)
+//                .headless(false)
+//                .bannerMode(Banner.Mode.OFF)
+//                .logStartupInfo(false)
+//                .run(args);
+
+
+		// Hier wird der ApplicationContext erzeugt.
+		ConfigurableApplicationContext context = SpringApplication.run(Date4uApplication.class, args);
+
+		// Zugriff auf alle managed Beans. ConfigurableApplicationContext erbt von ListableBeanFactory
+		List<String> allBeanNames = Arrays.stream(context.getBeanDefinitionNames()).sorted().toList();
+		boolean containsBean = context.containsBean("date4uApplication");
+		Date4uApplication date4uApplication = (Date4uApplication) context.getBean("date4uApplication");
+		date4uApplication = context.getBean("date4uApplication", Date4uApplication.class);
+		date4uApplication = context.getBean(Date4uApplication.class);
+
 	}
 
 }
